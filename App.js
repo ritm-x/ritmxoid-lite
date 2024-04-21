875dd600f800364f1d1313b28e8c760e25224314
Sergey Trofimov <sergey@trofimov.name>
2024-04-21 15:47:44 +0200

    (feat): set "light" status bar

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   );
 }
